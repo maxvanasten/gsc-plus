e2163e72f64124d91bf7f7ef0c1b8694df755189
maxvanasten <maxvasten@gmail.com>
2024-04-11 12:41:52 +0200

    feat: main logic boilerplate

---
+++ cmd/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Println("Usage: ./gscpp path/to/input.gpp")
+		os.Exit(2)
+	}
+
+	file_path := os.Args[1]
+
+	file_name_splits := strings.Split(file_path, "/")
+	file_name := file_name_splits[len(file_name_splits)-1]
+	file_name = strings.TrimSuffix(file_name, ".gpp")
+
+	output_file_path := "./output/gsc/" + file_name + ".gsc"
+
+	fmt.Println("FILENAME:", file_name)
+
+	input_bytes, err := os.ReadFile(file_path)
+	if err != nil {
+		fmt.Println("Error while reading file:", err)
+		os.Exit(1)
+	}
+}
